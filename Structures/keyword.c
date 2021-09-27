b98626e915186e0f460a95a96ce4b822784787e2
ChucklesDroid <mj.akarsh@gmail.com>
2021-09-27 22:21:28 +0530

    Removed bug where it would not stop taking input and would not display the count of the keywords

--- Structures/keyword.c
+++ Structures/keyword.c
@@ -49,16 +49,17 @@ int binsearch(char *, struct key *, int);
 /* count C keywords */
 int main() {
     int n;
-    char word[MAXWORD];
+    char word[MAXWORD], ch;
 
-    while (getword(word, MAXWORD) !='\n')
-        if (isalpha(word[0]))
-            if ((n = binsearch(word, keytab, NKEYS)) >= 0)
-                keytab[n].count++;
+    while ((ch = getword(word, MAXWORD)) !='\n' || ch != EOF ){ 
+    if (isalpha(word[0])) 
+        if ((n = binsearch(word, keytab, NKEYS)) >= 0)
+                keytab[n].count++;	
     for (n = 0; n < NKEYS; n++)
         if (keytab[n].count > 0)
             printf("%4d %s\n",
                    keytab[n].count, keytab[n].word);
+    }
     return 0;
 }
 
