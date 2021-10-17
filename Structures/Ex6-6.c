02e8dd652c4e5941e0893580f9aaa449d898c42a
ChucklesDroid <mj.akarsh@gmail.com>
2021-10-18 04:14:00 +0530

    Reconciling with Local Library

--- Structures/Ex6-6.c
+++ Structures/Ex6-6.c
@@ -9,6 +9,9 @@ struct hashtables{
 };
 
 unsigned directive = 0 ;
+unsigned key = 0; 
+unsigned value = 0;
+char *key , *value;
 struct hashtables tab[TABLESIZE] ;
 int getword( char *) ;
 void install( struct hashtables ** , ) ;
@@ -21,4 +24,26 @@ int main( int argc , char *argv[] )
 			install(tab,) ;
 	}
 	return 0 ;
+}
+
+int getword( char *word) 
+{
+	char *w = word , *k = key;
+	char ch ;
+	// Ignoring whitespaces
+	while( isspace(ch = getch()) ) ;	
+	
+	// Parsing preprocessor directive 
+	if( ch == '#' ){
+		*w = ch ;
+		while( isalpha( *w++ = ch = getch() ) ) ;
+		directive = ( strcmp(word , "#define" ) == 0 )? 1 : 0 ;
+		if( directive == 1 ) {
+			while( isspace( ch = getch() ) ;
+			while(isalpha( ch = *k++ = getch() ) || ch == '_') ;
+			
+		}
+		else
+			return word[0] ;
+	}
 }
