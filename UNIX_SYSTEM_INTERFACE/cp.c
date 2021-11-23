3bbd2ff7f52d035006ca050f528524d0ea19d417
ChucklesDroid <mj.akarsh@gmail.com>
2021-11-23 10:11:00 +0530

    Modified error function to incorporate variable argument list

--- UNIX_SYSTEM_INTERFACE/cp.c
+++ UNIX_SYSTEM_INTERFACE/cp.c
@@ -3,27 +3,39 @@
 #include <unistd.h> // For read and write
 #include <fcntl.h>  // For open and creat
 #include <stdlib.h> // For exit()
+#include <stdarg.h> // For variable argument list
 #define BUFFSIZE 100
 
 int buffer[BUFFSIZE] ;
-void error( char *error_msg ) ;
+//void error( char *error_msg ) ;
+void error( char *fmt , ... ) ;
 int main( int argc, char *argv[] )
 {
 	int nb_read , nb_written , f1 ,f2 ;
 	if( argc != 3 )
-		error("usage :cp from to ...\n\tmissing files ...\n\tTerminating the program now !!") ;
+		error("usage :cp from %s to %s...\n\tmissing files ...\n\tTerminating the program now !!",argv[1],argv[2]) ;
 	if( (f1 = open(argv[1] ,O_RDONLY,0755)) == -1 )
-		error("error encountered while opening file\n\tTerminating the program !!") ;
+		error("%s :error encountered while opening file \n\tTerminating the program !!",argv[1]) ;
 	if( (f2 = creat(argv[2],0666)) == -1 )
-		error("error encountered while creating file\n\t Terminating the program !!") ;
+		error("%s :error encountered while creating file\n\t Terminating the program !!",argv[2]) ;
 	while( (nb_read = read(f1,buffer,BUFFSIZE)) > 0)	
 		if( (nb_written = write(f2,buffer,BUFFSIZE)) != BUFFSIZE )
 			error("error encountered while copying file...\n\tOperation terminated \n\tFile not copied !!") ;
 	return 0 ;
 }
 
-void error( char *error_msg )
+/*void error( char *error_msg )
 {
 	fprintf(stderr,"%s",error_msg) ;
 	exit(1) ; //error encountered exiting the program now !!
+}*/
+
+/* variable argument list function : error() for displaying results */
+void error( char *fmt , ... )
+{
+	va_list ap ;
+	va_start(ap,fmt) ;
+	vfprintf(stderr,fmt,ap) ;
+	va_end(ap) ;
+	exit(1) ;	
 }
